63ca82ff53c78069e9f622465ee9ff9231a4965b
Jks Liu <chinatianma@gmail.com>
2013-12-19 20:54:27 +0800

    re construct

--- ds18b20-c51-code-autogeneration.js
+++ ds18b20-c51-code-autogeneration.js
@@ -8,19 +8,29 @@ function main() {
     document.getElementById('djnz time ns echo').innerHTML =
 	'Executing \`DJNZ\' needs <strong>' + djnz_time_ns + '</strong> nanosecond(s).';
 
-    generate_delay_1ms(djnz_time_ns);
+    generate_delay(1e6, 'delay_1ms', djnz_time_ns);
 }
 
-function generate_delay_1ms(ns) {
-    var count = Math.ceil(1e6 / ns) - 2; // Two `MOVE', one `RET'
+function generate_delay(time_ns, id, djnz_time_ns) {
+
+    var count = Math.ceil(time_ns / djnz_time_ns - 0.5); // Inherent delay
+    if (count <= 0) {
+	document.getElementById(id).innerHTML = '; No delay here';
+    }
     var quotient = Math.floor(count / 256);
     var remainder = count % 256;
-    
-    document.getElementById('delay 1ms').innerHTML = 
-	'    MOV R6, ' + (quotient + 1) + '\n' +
-	'    MOV R7, ' + remainder + '\n' +
-	'delay_ms_loop:' + '\n' +
-	'    DJNZ R7, $' + '\n' +
-	'    DJNZ R6, delay_ms_loop' + '\n' +
-	'    RET'
+
+    if (quotient == 0) {
+	document.getElementById(id).innerHTML = 
+	    '    MOV R7, #' + remainder + '\n' +
+	    '    DJNZ R7, $';
+
+    } else {
+	document.getElementById(id).innerHTML = 
+	    '    MOV R6, #' + (quotient + 1) + '\n' +
+	    '    MOV R7, #' + remainder + '\n' +
+	    id + '_loop:' + '\n' +
+	    '    DJNZ R7, $' + '\n' +
+	    '    DJNZ R6, ' + id + '_loop'
+    }
 }
